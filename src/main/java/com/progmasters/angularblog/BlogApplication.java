b1a93c7fda11c9d7249ce422f58cb76975ed9b6b
Szabi <filep.szabolcs@apertus.uni-nke.hu>
2020-08-22 21:39:18 +0200

    rename react to angularblog

--- src/main/java/com/progmasters/angularblog/BlogApplication.java
+++ src/main/java/com/progmasters/angularblog/BlogApplication.java
@@ -1,4 +1,4 @@
-package com.progmasters.reactblog;
+package com.progmasters.angularblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

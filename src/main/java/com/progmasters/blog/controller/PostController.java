e7b095209ecedd5988ac623d32e53f5b012e137b
kristof.csef@gmail.com <kristof.csef@gmail.com>
2020-08-24 18:11:37 +0200

    working on validation

--- src/main/java/com/progmasters/blog/controller/PostController.java
+++ src/main/java/com/progmasters/blog/controller/PostController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -26,7 +27,7 @@ public class PostController {
     }
 
     @PostMapping
-    public ResponseEntity<Void> createPost(@RequestBody PostFormItem postFormItem) {
+    public ResponseEntity<Void> createPost(@Valid @RequestBody PostFormItem postFormItem) {
         postService.createPost(postFormItem);
         return new ResponseEntity<>(HttpStatus.OK);
     }
